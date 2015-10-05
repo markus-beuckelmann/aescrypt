6cd91da42bae79732af65aa55e6db06746a608c4
Markus Beuckelmann <email@markus-beuckelmann.de>
2015-10-05 19:03:45 +0200

    Add __init__.py

---
+++ aescrypt/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2
+# -*- coding: UTF-8 -*-
+
+__author__ = 'Markus Beuckelmann'
+__author_email__ = 'email@markus-beuckelmann.de'
+__version__ = '0.1.0'
+
+from .aescrypt import encrypt, decrypt, pad, unpad
+del aescrypt
